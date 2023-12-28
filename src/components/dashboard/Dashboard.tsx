b381fa4757bd2130a6b78267cfc1917348400dba
Walassi Silva <walassi.b@gmail.com>
2023-12-28 18:30:05 -0300

    add products and order sections

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { DashboardStatsGrid } from "./DashboardStatsGrid";
 import { TransactionChart } from "../charts/TransactionChart";
 import { BuyerChart } from "../charts/BuyerChart";
+import { RecentOrder } from "../products/RecentOrder";
+import { PopularProducts } from "../products/PolularProducts";
 
 const Dashboard = () => {
     return (
@@ -10,6 +12,11 @@ const Dashboard = () => {
             <div className="flex flex-row gap-4 w-full">
                 <TransactionChart />
                 <BuyerChart />
+            </div>
+
+            <div className="flex flex-row gap-4 w-full">
+                <RecentOrder />
+                <PopularProducts />
             </div>
 
             <Link to="/products" className="underline">Go to Projects</Link>
